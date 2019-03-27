6d586cf28b91e2fa68624677bf6ae99e6131efb0
Maciej Stepien <maciek1284@outlook.com>
2019-03-27 20:32:46 +0100

    upubliczniona cewka tesli

---
+++ _posts/2015-12-10-cewkatesli.md
@@ -0,0 +1,23 @@
+---
+layout: post
+title: Cewka Tesli
+categories:
+- Elektronika
+---
+
+Robi się groźnie.
+
+{% include figure.html image="/pics/cewkatesli/wieksza.jpg" width="600" height="800" %}
+
+Projekt cewki tesli wykonany na podstawie 
+[ElectroBoom](https://www.electroboom.com/?p=521 "ElectroBoom")
+oraz
+[Ludic Science](https://www.youtube.com/watch?v=4OC7cwI4RNM "Ludic Science")
+
+{% include figure.html image="/pics/cewkatesli/oryginal.jpg" width="400" height="800" %}
+
+Najpierw wykonałem mniejszą wersję opartą na jednym tranzystorze, zasilaną baterią 9V.
+
+{% include figure.html image="/pics/cewkatesli/nawijanie.jpg" width="600" height="800" caption="Nawijanie cewki"%}
+
+Następnie wykonałem cewkę o większych rozmiarach, w ktrórej zastosowałem 4 tranzystory oraz dodatkowy radiator, aby zapewnić odpowiednie chłodzenie. Jako źródło energii użyłem zasilacza od drukarki.
--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20190327200454';
+const version = '20190327203007';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/roboty/2019/03/27/tank/","/roboty/2018/06/08/hexapod/","/roboty/2015/08/25/mapping-robot/","/oprogramowanie/2015/04/09/3dviewer/","/roboty/2014/08/21/universal-robots/","/oprogramowanie/2014/01/18/2dracing/","/oprogramowanie/2013/09/23/Blu/","/oprogramowanie/2013/05/31/gry-konsolowe/","/about/","/categories/","/projekty/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/blog/page2/","/feed.xml","/pics/logo1.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/roboty/2019/03/27/tank/","/roboty/2018/06/08/hexapod/","/elektronika/2015/12/10/cewkatesli/","/roboty/2015/08/25/mapping-robot/","/oprogramowanie/2015/04/09/3dviewer/","/roboty/2014/08/21/universal-robots/","/oprogramowanie/2014/01/18/2dracing/","/oprogramowanie/2013/09/23/Blu/","/oprogramowanie/2013/05/31/gry-konsolowe/","/about/","/categories/","/projekty/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/blog/page2/","/feed.xml","/pics/logo1.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
